671983d4ed1fdea86ee053d6d568766c23a0d6e5
E. Maric <emaric.dev@gmail.com>
2022-04-26 15:10:13 +0800

    :lipstick: set volume to 0.5

--- gittodoro-next/src/components/clock/audios.tsx
+++ gittodoro-next/src/components/clock/audios.tsx
@@ -9,6 +9,12 @@ export const AudioStart = ({ play = false }) => {
     }
   }, [play])
 
+  useEffect(() => {
+    if (audioStartRef.current) {
+      audioStartRef.current.volume = 0.5
+    }
+  }, [audioStartRef])
+
   return (
     <audio ref={audioStartRef}>
       <source src='/audio/clockworkwindup2-quicksounds.com.mp3' />
@@ -24,6 +30,12 @@ export const AudioEnd = ({ play = false }) => {
       audioEndRef.current?.play()
     }
   }, [play])
+
+  useEffect(() => {
+    if (audioEndRef.current) {
+      audioEndRef.current.volume = 0.5
+    }
+  }, [audioEndRef])
 
   return (
     <audio ref={audioEndRef}>
@@ -47,8 +59,14 @@ export const AudioTick = ({ playOnLoop = false }) => {
     evaluateReplay()
   }, [evaluateReplay])
 
+  useEffect(() => {
+    if (audioTickRef.current) {
+      audioTickRef.current.volume = 0.5
+    }
+  }, [audioTickRef])
+
   return (
-    <audio ref={audioTickRef} loop={true} >
+    <audio ref={audioTickRef} loop={true}>
       <source src='/audio/clocktickingloop-quicksounds.com.mp3' />
     </audio>
   )
