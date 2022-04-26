c8d940f4f2daeebaff0cec37e62ae1fc677b6729
E. Maric <emaric.dev@gmail.com>
2022-04-26 08:28:05 +0800

    :heavy_minus_sign: undo webworkers; encountered issue with loading workers.

--- gittodoro-next/next.config.js
+++ gittodoro-next/next.config.js
@@ -1,16 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const WorkerPlugin = require('worker-plugin')
-
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config, { buildId, dev }) => {
     config.resolve.symlinks = false
-    config.plugins.push(
-      new WorkerPlugin({
-        globalObject: 'self',
-      })
-    )
     return config
   },
 }
